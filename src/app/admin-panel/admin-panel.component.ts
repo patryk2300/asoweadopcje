0ee4a5308bc8a4b015aa90afae4053a64e1a83b7
patryk2300 <patryk2300@gmail.com>
2019-03-13 17:11:10 +0100

    first version of calendar

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { ManageGalleryCardComponent } from '../manage-gallery-card/manage-gallery-card.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-admin-panel',
+  selector: 'admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrls: ['./admin-panel.component.scss']
 })
-export class AdminPanelComponent implements OnInit {
+export class AdminPanelComponent {
 
-  constructor() { }
+  openedSidebar: boolean;
 
-  ngOnInit() {
+  constructor(private user: AuthService, private modalService: NgbModal) {}
+
+  toggleSidebar() {
+
+    if(this.openedSidebar){
+      document.getElementById('mySidebar').style.width = "0px";
+      document.getElementById('main').style.marginLeft = "0px";
+    } else{
+      document.getElementById('mySidebar').style.width = "250px";
+      document.getElementById('main').style.marginLeft = "250px";
+    }
+
+    this.openedSidebar = !this.openedSidebar;
   }
 
+  openFormModal() {
+    const modalRef = this.modalService.open(ManageGalleryCardComponent);
+
+    modalRef.result.then((result) => {
+      console.log(result);
+    }).catch((error) => (console.log(error)));
+  }
 }
