3cdd6f314f704c520f240ed0253606ac646e593d
patryk2300 <patryk2300@gmail.com>
2019-03-20 18:51:54 +0100

    adding new calendar with modern look, and possibility of adding events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { GalleryComponent } from './gallery/gallery.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { DescriptionComponent } from './description/description.component';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { FileUploadComponent } from './file-upload/file-upload.component';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
-import { ManageGalleryCardComponent } from './manage-gallery-card/manage-gallery-card.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FileDropDirective } from './file-drop.directive';
-import { DisplayImageDirective } from './display-image.directive';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { FooterComponent } from './footer/footer.component';
+import { ManageGalleryCardComponent } from './manage-gallery-card/manage-gallery-card.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+
+import { FileDropDirective } from './file-drop.directive';
 
 
 @NgModule({
@@ -42,7 +43,6 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
     AdminPanelComponent,
     ManageGalleryCardComponent,
     FileDropDirective,
-    DisplayImageDirective,
     ScheduleComponent,
     FooterComponent,
   ],
@@ -62,7 +62,7 @@ import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
       NgxMaterialTimepickerModule,
     ]
   ],
-  providers: [FileDropDirective, DisplayImageDirective],
+  providers: [FileDropDirective],
   bootstrap: [AppComponent],
   entryComponents: [
     ManageGalleryCardComponent
--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EventsService } from '../services/events.service';
 
 @Component({
   selector: 'schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.scss']
 })
-export class ScheduleComponent {
+export class ScheduleComponent implements OnInit{
+
+  events: Array<any>;
+  eventsAmount: Array<number> = new Array(12);
+  eventsInDay: Array<Array<any>> = new Array(24);
 
   date = new Date();
   
@@ -13,18 +18,69 @@ export class ScheduleComponent {
             'Kwiecień', 'Maj', 'Czerwiec', 
             'Lipiec', 'Sierpień', 'Wrzesień', 
             'Październik', 'Listopad', 'Grudzień'];
+  
   daysOfWeek = ['Pn','Wt','Śr','Czw','Pt','Sb','Nd'];
 
   days: Array<any>;
-  currentDay: number;
+  currentDay: number = 0;
   currentMonth: number;
   currentYear: number;
+
+  currentColorId: string;
   currentColor: string;
 
-  constructor() {
+  timeValue: string;
+  eventDesc: string;
+
+  toAddEvent: boolean = false;
+
+
+  constructor(private eventService: EventsService) {
     this.currentMonth = this.date.getMonth();
     this.currentYear = this.date.getFullYear();
     this.days = new Array(this.daysInMonth());
+    this.events = new Array(this.daysInMonth());
+  }
+
+  ngOnInit(): void {
+    this.getEventsInYear()
+    this.getEventsInMonth();
+    this.getEventsInDay();
+  }
+
+  getEventsInYear(){
+    for (let index = 0; index < 12; index++) {
+      this.eventService.getEvents(this.currentYear, index)
+        .subscribe(events => {
+          this.eventsAmount[index] = events.length;
+        });
+    }
+  }
+
+  getEventsInMonth(){
+    this.eventService.getEvents(this.currentYear, this.currentMonth)
+      .subscribe(events => {
+        events.forEach((value) => {
+          for (const key in value) {
+            if (value.hasOwnProperty(key)) {
+              this.days[value[key].eventDay] = value[key];
+              this.events[value[key].eventDay] = value;
+            }
+          }
+        });
+      });
+  }
+
+  getEventsInDay(){
+    for (let index = 0; index < this.eventsInDay.length; index++) {
+      this.eventsInDay[index] = []; 
+    }
+
+    for (const key in this.events[this.currentDay]){
+      const event = this.events[this.currentDay][key]
+      const eventTime = Math.floor(event.eventTime.split(':').join('.'));
+      this.eventsInDay[eventTime].push(event);
+    }
   }
 
   daysInMonth(){
@@ -33,65 +89,123 @@ export class ScheduleComponent {
 
   changeYear(dir: number){
     this.currentYear += dir;
+    this.getEventsInYear();
+    this.changeMonth(this.months[this.currentMonth]);
+    this.exitEvent('set-event');
+  }
+
+  getIndexOfMonth(month: string){
+    return this.months.indexOf(month);
   }
 
   changeMonth(month: string){
-    this.currentMonth = this.months.indexOf(month);
+    this.currentMonth = this.getIndexOfMonth(month);
+    
     this.days = new Array(this.daysInMonth());
+    this.events = new Array(this.daysInMonth());
+
+    this.getEventsInMonth();
+    this.exitEvent('set-event');
   }
 
   chooseElement(color: string){
-    let elmName = 'table-element' + this.currentDay;
-    document.getElementById(elmName).style.backgroundColor = color;
-    document.getElementById(elmName).style.borderRadius = '100%';
-
-    // document.getElementById('event-menu').style.display = 'block';
+    this.days[this.currentDay] = {'color': color};
   }
 
-  addEvent(day: number){
+  chooseDay(day: number){
     this.currentDay = day;
 
-    let elmName = 'table-element' + day;
-    let elm = document.getElementById(elmName);
-    let eventMenu = document.getElementById('set-event');
-    let schedule = document.getElementById('schedule');
+    this.onResize();
+    this.getEventsInDay();
+    
+    const eventSchedule = document.getElementById('event-menu');
+    const eventMenu = document.getElementById('set-event');
+    
+    if(!this.days[this.currentDay] && eventMenu.hidden)
+      eventMenu.hidden = false;
+    
+    else if(this.days[this.currentDay] && eventSchedule.hidden){
+      eventSchedule.hidden = false;
+      eventMenu.hidden = true;
+    }
 
+    else if(this.days[this.currentDay] && eventMenu.hidden)
+      eventMenu.hidden = false;
+    
+    else
+      eventMenu.hidden = true;
+  }
 
-    let leftValue = (elm.offsetLeft + (elm.offsetWidth / 2) - schedule.offsetLeft - 125).toString() + 'px';
-    let topValue = (elm.offsetTop  - schedule.offsetTop - 325).toString() + 'px';
-
-    eventMenu.style.left = leftValue;
-    eventMenu.style.top = topValue;
-    eventMenu.style.visibility = 'visible';
+  onResize(){
+    if(this.currentDay >= 0){
+      
+      let elmName = 'table-element' + this.currentDay;
+      let elm = document.getElementById(elmName);
+      let eventMenu = document.getElementById('set-event');
+      let schedule = document.getElementById('schedule');
+  
+  
+      let leftValue = (elm.offsetLeft + (elm.offsetWidth / 2) - schedule.offsetLeft - 125).toString() + 'px';
+      let topValue = (elm.offsetTop  - schedule.offsetTop - 325).toString() + 'px';
+  
+      eventMenu.style.left = leftValue;
+      eventMenu.style.top = topValue;
+    }
   }
 
   exitEvent(eventName: string){
     if(eventName === 'event-menu')
-      document.getElementById('event-menu').style.display = 'none';
-    else if(eventName === 'set-event')
-      document.getElementById('set-event').style.visibility = 'hidden';
+      document.getElementById('event-menu').hidden = true;
+    else if(eventName === 'set-event'){
+      document.getElementById('set-event').hidden = true;
+      this.clear();
+    }
   }
 
-  openModal(day: number){
-    document.getElementById('scheduleModal').style.display = 'block';
-  }
-
-  closeModal(){
-    document.getElementById('scheduleModal').style.display = 'none';
+  clear() {
+    this.timeValue = '';
+    this.eventDesc = '';
+    
+    if(this.currentColorId)
+      document.getElementById(this.currentColorId).style.boxShadow = 'none';
+    
+    if(!this.days[this.currentDay])
+      this.chooseElement('transparent');
   }
 
   setColor(id: string, color: string){
-    if(!this.currentColor)
-      document.getElementById(id).style.webkitBoxShadow = '0px 0px 3px 3px green';
+    if(!this.currentColorId)
+      document.getElementById(id).style.boxShadow = '0px 0px 3px 3px green';
     
     
-    else if(id != this.currentColor){
-      document.getElementById(id).style.webkitBoxShadow = '0px 0px 3px 3px green';
-      document.getElementById(this.currentColor).style.webkitBoxShadow = 'none';
+    else if(id != this.currentColorId){
+      document.getElementById(id).style.boxShadow = '0px 0px 3px 3px green';
+      document.getElementById(this.currentColorId).style.boxShadow = 'none';
     }
     
-    this.currentColor = id;
+    this.currentColorId = id;
+    this.currentColor = color;
 
     this.chooseElement(color);
+  }
+
+  addEvent(){
+    let newEvent = {};
+    let key = this.date.getTime().toString() + Math.floor((Math.random() * 1000000)).toString();
+    
+    newEvent[key] = {
+      'eventMonth': this.currentMonth,
+      'eventDay': this.currentDay,
+      'eventYear': this.currentYear,
+      'eventTime': this.timeValue,
+      'eventDesc': this.eventDesc,
+      'color': this.currentColor
+    }
+
+    this.eventService.pushEvent(newEvent, this.currentYear, this.currentMonth, this.currentDay)
+        .then(() => {
+          this.exitEvent('set-event');
+          this.getEventsInMonth();
+        });
   }
 }
